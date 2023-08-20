88ab225d218a378b3016dc56c134b5461061e3dd
pjancura <jancura.paul@gmail.com>
2023-08-20 13:10:48 -0400

    fixes class typo

--- first-app-lesson-00/src/app/housing-location/housing-location.component.ts
+++ first-app-lesson-00/src/app/housing-location/housing-location.component.ts
@@ -9,7 +9,7 @@ import { HousingLocation } from  '../housinglocation';
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
-      <h2 class="llisting-heading">{{ housingLocation.name }}</h2>
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{ housingLocation.state }}</p>
     </section>
   `,
