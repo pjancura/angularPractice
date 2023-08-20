5902df018fb1af67105384a766f4dea4b084b015
pjancura <jancura.paul@gmail.com>
2023-08-20 16:50:18 -0400

    finishes angular tut

--- first-app-lesson-00/src/app/housing-location/housing-location.component.ts
+++ first-app-lesson-00/src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from  '../housinglocation';
+import { Router, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   template: `
     <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
+      <img class="listing-photo" src="https://angular.io/assets/images/tutorials/faa{{housingLocation.photo}}" alt="Exterior photo of {{ housingLocation.name }}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
